a2fb69e98837a2d3461d486928751085c550e47a
emersonCK <56833594+emersonCK@users.noreply.github.com>
2022-09-10 12:16:18 -0300

    feature: aula 10/02/2022

--- resources/views/site/contatos.blade.php
+++ resources/views/site/contatos.blade.php
@@ -1,12 +1,33 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contatos</title>
-</head>
-<body>
-    <h1>Contatos</h1>
-</body>
-</html>
+@extends('site.master.layout')
+
+@section('content')
+
+<div class="jumbotron">
+    <h1 class="display-4">Contatos</h1>
+    <hr class="my-4">
+    <p class="lead">Nossa equipe está sempre a disposição para ouvir as suas críticas e sugestões. Entre em contato que iremos responder o mais breve posível.</p>
+</div>
+
+<div class="container">
+    <form>
+        <div class="form-group">
+          <label for="emailContato">Email:</label>
+          <input type="email" class="form-control" id="emailContato" name="emailContato" placeholder="nome@email.com">
+        </div>
+        <div class="form-group">
+          <label for="tipoContato">Tipo de contato:</label>
+          <select class="form-control" id="tipoContato" name="tipoContato">
+            <option value="Sugestão">Sugestão</option>
+            <option value="Crítica">Crítica</option>
+            <option value="Elogio">Elogio</option>
+            <option value="Dúvida">Dúvida</option>
+          </select>
+        </div>
+        <div class="form-group">
+          <label for="mensagemContato">Mensagem:</label>
+          <textarea class="form-control" id="mensagemContato" name="mensagemContato" rows="3"></textarea>
+          <button type="submit" class="btn btn-primary mt-2">Enviar</button>
+        </div>
+    </form>
+</div>
+@endsection
